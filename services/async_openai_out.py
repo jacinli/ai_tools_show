b54feea929f6afacd02c04e1a0389fbf96358f5c
jacinli <jackleo120ch@gmail.com>
2025-03-22 15:22:31 +0800

    Add .env.example for environment variable configuration and update AsyncOpenAIOut to use OPENAI_MODEL from environment variables.

--- services/async_openai_out.py
+++ services/async_openai_out.py
@@ -10,8 +10,8 @@ class AsyncOpenAIOut:
         self.api_key = os.getenv("OPENAI_API_KEY")
         self.base_url = os.getenv("OPENAI_BASE_URL")
         self.oai_client = AsyncOpenAI(api_key=self.api_key, base_url=self.base_url)
-
-    async def gpt_stream(self, user_message: str,model: str = "gpt-4o-mini",history: list[dict] = [],system_prompt: str = "") :
+        self.model = os.getenv("OPENAI_MODEL")
+    async def gpt_stream(self, user_message: str,model: str = os.getenv("OPENAI_MODEL"),history: list[dict] = [],system_prompt: str = "") :
         messages = []
         if history:
             messages.extend(history)
@@ -34,11 +34,12 @@ async_openai_out = AsyncOpenAIOut()
 
 
 
-
+    
 if __name__ == "__main__":
     async def test_gpt_stream():
         async for chunk in async_openai_out.gpt_stream(user_message="写300字作文",system_prompt="You are a helpful assistant."):
-            print(chunk)
+            # print(chunk)
+            await asyncio.to_thread(print, chunk)  # 在后台线程执行 print
 
     import asyncio
     
